d5c5a8bff06ecefcb4db0eebb865b493e4903d5f
Satyam Singh <satyameducational0906@gmail.com>
2026-06-08 16:59:49 +0530

    day12

---
+++ day 12/waptoarmstrongfuction.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int armstrong(int n)
+{
+    int sum = 0, temp = n, rem;
+
+    while (temp > 0)
+    {
+        rem = temp % 10;
+        sum += rem * rem * rem;
+        temp /= 10;
+    }
+
+    return (sum == n);
+}
+
+int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (armstrong(num))
+        printf("Armstrong Number");
+    else
+        printf("Not an Armstrong Number");
+
+    return 0;
+}
